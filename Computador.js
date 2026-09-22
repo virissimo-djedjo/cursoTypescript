2fa45bcf38ffbdeb67a6c29d3df9bb76375c7e93
virissimo-djedjo <djedjovirissimo450@gmail.com>
2026-09-22 17:12:37 -0300

    adicionar os metodos de ligar e desligar o computador

--- Computador.js
+++ Computador.js
@@ -11,12 +11,24 @@ class Computador {
         this.cpu = cpu;
         this.ligado = ligado;
     }
+    informacao() {
+        console.log("Nome: " + this.nome);
+        console.log("Ram: " + this.ram);
+        console.log("CPU: " + this.cpu);
+        console.log("Ligado: " + this.ligado);
+        console.log("------------------------");
+    }
+    ligar() {
+        this.ligado = true;
+    }
+    desligar() {
+        this.ligado = false;
+    }
 }
 const comp1 = new Computador("Sansung", 16, 128.5, true);
 const comp2 = new Computador("Apple", 32, 124.3, false);
 const comp3 = new Computador("Microsoft", 8, 134.5, true);
-console.log(comp1.nome);
-console.log(comp1.ram);
-console.log(comp1.cpu);
-console.log(comp1.ligado);
+comp1.informacao();
+comp2.informacao();
+comp3.informacao();
 //# sourceMappingURL=Computador.js.map
