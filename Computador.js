1eb505eae6f9cae53e668938d1f46d3e79a1e1b7
virissimo-djedjo <djedjovirissimo450@gmail.com>
2026-09-22 17:02:01 -0300

    construção de constructor

--- Computador.js
+++ Computador.js
@@ -5,10 +5,18 @@ class Computador {
     ram = 8;
     cpu = 16;
     ligado = true;
-    constructor() {
+    constructor(nome, ram, cpu, ligado) {
+        this.nome = nome;
+        this.ram = ram;
+        this.cpu = cpu;
+        this.ligado = ligado;
     }
 }
-const comp1 = new Computador();
-const comp2 = new Computador();
-const comp3 = new Computador();
+const comp1 = new Computador("Sansung", 16, 128.5, true);
+const comp2 = new Computador("Apple", 32, 124.3, false);
+const comp3 = new Computador("Microsoft", 8, 134.5, true);
+console.log(comp1.nome);
+console.log(comp1.ram);
+console.log(comp1.cpu);
+console.log(comp1.ligado);
 //# sourceMappingURL=Computador.js.map
